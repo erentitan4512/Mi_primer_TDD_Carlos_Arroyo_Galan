7611b87187c54fa043e45073c51293c9e524bc5f
Carlos <erentitan4512@gmail.com>
2023-06-11 15:42:31 +0200

    Fourth Commit

--- tests/Coche.java
+++ tests/Coche.java
@@ -4,7 +4,9 @@ public class Coche {
     public void acelerar(int aceleración) {
         velocidad += aceleración;
     }
+
     public void decelerar(int deceleración) {
         velocidad -= deceleración;
+        if (velocidad <0) velocidad = 0;
     }
 }
